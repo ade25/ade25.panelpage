45edee6cabe12c1ce578e09aa41474ad4b1197b7
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-01-30 00:09:39 +0100

    Temporary commit prior merge with upstream

--- ade25/panelpage/panelpage.py
+++ ade25/panelpage/panelpage.py
@@ -139,7 +139,52 @@ class PanelPageEditor(grok.View):
     grok.name('panelpage-editor')
 
     def update(self):
+        context = aq_inner(self.context)
         self.has_subcontent = len(self.contained_blocks()) > 0
+        self.errors = {}
+        unwanted = ('_authenticator', 'form.button.Submit')
+        required = ('title')
+        if 'form.button.Submit' in self.request:
+            authenticator = getMultiAdapter((context, self.request),
+                                            name=u"authenticator")
+            if not authenticator.verify():
+                raise Unauthorized
+            form = self.request.form
+            form_data = {}
+            form_errors = {}
+            errorIdx = 0
+            for value in form:
+                if value not in unwanted:
+                    form_data[value] = safe_unicode(form[value])
+                    if not form[value] and value in required:
+                        error = {}
+                        error['active'] = True
+                        error['msg'] = _(u"This field is required")
+                        form_errors[value] = error
+                        errorIdx += 1
+                    else:
+                        error = {}
+                        error['active'] = False
+                        error['msg'] = form[value]
+                        form_errors[value] = error
+            if errorIdx > 0:
+                self.errors = form_errors
+            else:
+                self._create_panel(form)
+
+    def _create_panel(self, data):
+        context = aq_inner(self.context)
+        new_title = data['title']
+        token = django_random.get_random_string(length=12)
+        api.content.create(
+            type='ade25.panelpage.contentblock',
+            id=token,
+            title=new_title,
+            container=context,
+            safe_id=True
+        )
+        url = context.absolute_url() + '/@@panelpage-editor'
+        return self.request.response.redirect(url)
 
     def render_item(self, uid):
         item = api.content.get(UID=uid)
