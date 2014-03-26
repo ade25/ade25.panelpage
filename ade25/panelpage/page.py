a9bdb138a872742cab380f65e3a9a0ca634c5cf7
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-03-26 23:04:24 +0100

    Refactor page content type

--- ade25/panelpage/page.py
+++ ade25/panelpage/page.py
@@ -1,24 +1,13 @@
 from Acquisition import aq_inner
 from five import grok
 
-from z3c.form import group, field
 from zope import schema
-from zope.interface import invariant, Invalid
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
 from plone.indexer import indexer
 from plone.dexterity.content import Container
 
-from plone.directives import dexterity, form
-from plone.app.textfield import RichText
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+from plone.directives import form
 from plone.namedfile.interfaces import IImageScaleTraversable
-
-from z3c.relationfield.schema import RelationList, RelationChoice
-from plone.formwidget.contenttree import ObjPathSourceBinder
-
 
 from ade25.panelpage import MessageFactory as _
 
@@ -27,6 +16,11 @@ class IPage(form.Schema, IImageScaleTraversable):
     """
     A modular ppage with panel layout
     """
+    form.fieldset(
+        'overrides',
+        label=_(u"Overrides"),
+        fields=['headline', 'abstract']
+    )
     headline = schema.TextLine(
         title=_(u"Headline"),
         description=_(u"Optional headline overiding the default title in the "
@@ -37,12 +31,6 @@ class IPage(form.Schema, IImageScaleTraversable):
         title=_(u"Abstract"),
         description=_(u"Optional abstract for the page content overriding the "
                       u"Dublin Core description"),
-        required=False,
-    )
-    text = RichText(
-        title=_(u"Body Text"),
-        description=_(u"Optional rich body text. Note: you can use content "
-                      u"blocks to structure the page body"),
         required=False,
     )
 
@@ -74,13 +62,3 @@ class View(grok.View):
         if context.Description or context.abstract:
             show = True
         return show
-
-
-class AddForm(dexterity.AddForm):
-    grok.name('ade25.panelpage.page')
-
-    def updateWidgets(self):
-        super(AddForm, self).updateWidgets()
-        self.widgets['headline'].mode = 'hidden'
-        self.widgets['abstract'].mode = 'hidden'
-        self.widgets['text'].mode = 'hidden'
