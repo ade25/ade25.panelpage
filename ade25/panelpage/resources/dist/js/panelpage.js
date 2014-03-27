820bcef4786bc7f20d7de0ce3159d2903ce647ca
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-03-27 17:43:08 +0100

    Update panelpage block editor view

--- ade25/panelpage/resources/dist/js/panelpage.js
+++ ade25/panelpage/resources/dist/js/panelpage.js
@@ -93,7 +93,6 @@ $.fn.sortable = function(options) {
 };
 })(jQuery);
 
-/*jslint white:false, onevar:true, undef:true, nomen:true, eqeqeq:true, plusplus:true, bitwise:true, regexp:true, newcap:true, immed:true, strict:false, browser:true */
 /*global jQuery:false, document:false */
 'use strict';
 
@@ -106,16 +105,16 @@ $.fn.sortable = function(options) {
         });
         if ($sortableSection.length) {
             $sortableSection.on('sortupdate', function () {
-                var $ajaxTarget = $sortableSection.data('appui-ajax-uri');
-                var $data = $('#ppe-form-rearrange').serializeArray();
+                var $ajaxTarget = $sortableSection.data('appui-ajax-uri'),
+                    $data = $('#ppe-form-rearrange').serializeArray();
                 $.ajax({
                     url: $ajaxTarget,
                     data: $data,
                     context: document.body,
                     success: function (data) {
                         if (data.success === true) {
-                            var $message = data.message;
-                            var $htmlString = '<p class="text-warning">' + $message + '</p>';
+                            var $message = data.message,
+                                $htmlString = '<p class="text-warning">' + $message + '</p>';
                             $('#ppe-statusinfo').append($htmlString).removeClass('hide').slideDown('slow');
                         } else {
                             // This could be nicer in the future...
@@ -126,5 +125,5 @@ $.fn.sortable = function(options) {
             });
         }
     }
-    );
+        );
 }(jQuery));
