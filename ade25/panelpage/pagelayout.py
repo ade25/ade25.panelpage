606c5bcfc2e070145070ed354b9a6cc3556169da
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-05-09 10:58:00 +0200

    Add migration view for existing blocks
    This should make sure that we at least keep all the contents already
    restructured when moving to the new architecture

--- ade25/panelpage/pagelayout.py
+++ ade25/panelpage/pagelayout.py
@@ -1,17 +1,20 @@
-from AccessControl import Unauthorized
 from Acquisition import aq_inner
 from five import grok
+from plone import api
 from zope import schema
 
-from zope.component import getMultiAdapter
 from zope.lifecycleevent import modified
 from zope.interface import alsoProvides
 from plone.directives import form
+from plone.keyring import django_random
 
 from plone.autoform.interfaces import IFormFieldProvider
+from plone.app.layout.navigation.interfaces import INavigationRoot
 from Products.statusmessages.interfaces import IStatusMessage
 
+from ade25.panelpage.contentblock import IContentBlock
 from ade25.panelpage.panelpage import IPanelPage
+
 
 from ade25.panelpage import MessageFactory as _
 
@@ -31,29 +34,96 @@ class IPanelPageLayout(form.Schema):
 alsoProvides(IPanelPageLayout, IFormFieldProvider)
 
 
-class LayoutChanger(grok.View):
-    grok.context(IPanelPage)
+class MigrateLayout(grok.View):
+    grok.context(INavigationRoot)
     grok.require('cmf.ManagePortal')
-    grok.name('page-layout')
+    grok.name('migrate-layout')
 
-    def update(self):
-        context = aq_inner(self.context)
-        if 'form.button.Submit' in self.request:
-            authenticator = getMultiAdapter((context, self.request),
-                                            name=u"authenticator")
-            if not authenticator.verify():
-                raise Unauthorized
-            form = self.request.form
-            self.applyLayout(form)
+    def render(self):
+        pages = self._panel_pages()
+        updated = self._migrate_items(pages)
+        msg = '{0} blocks migrated to panels'.format(updated)
+        IStatusMessage(self.request).addStatusMessage(msg, type='info')
+        url = self.context.absolute_url()
+        return self.request.response.redirect(url)
 
-    def applyLayout(self, data):
+    def _panel_pages(self):
+        catalog = api.portal.get_tool(name='portal_catalog')
+        items = catalog(object_provides=IPanelPage.__identifier__,)
+        return items
+
+    def _migrate_items(self, pages):
         context = aq_inner(self.context)
-        updated = data['layout']
-        setattr(context, 'panelPageLayout', updated)
+        catalog = api.portal.get_tool(name="portal_catalog")
+        idx = 0
+        for page in pages:
+            grid = list()
+            obj = page.getObject()
+            blocks = catalog(object_provides=IContentBlock.__identifier__,
+                             path=dict(query='/'.join(obj.getPhysicalPath()),
+                                       depth=1))
+            for b in blocks:
+                cb = b.getObject()
+                title_row = self._build_gridrow(cb, 'title')
+                grid.append(title_row)
+                idx += 1
+                if cb.Description:
+                    abstract_row = self._build_gridrow(cb, 'description')
+                    grid.append(abstract_row)
+                    idx += 1
+                if cb.text:
+                    text_row = self._build_gridrow(cb, 'text')
+                    grid.append(text_row)
+                    idx += 1
+        context = aq_inner(self.context)
+        setattr(context, 'panelPageLayout', grid)
         modified(context)
         context.reindexObject(idxs='modified')
-        IStatusMessage(self.request).addStatusMessage(
-            _(u"The content block has successfully been updated"),
-            type='info')
-        url = '{0}/@@panelpage-editor'.format(context.absolute_url())
-        return self.request.response.redirect(url)
+        return idx
+
+    def _build_gridrow(self, cb, key):
+        value = getattr(cb, key)
+        token = django_random.get_random_string(length=24)
+        component = u"text"
+        if key == 'headline':
+            component = u"heading"
+        if key == 'description':
+            component = u"abstract"
+        uid = self._create_panel(component, key, value)
+        row = {
+            'id': token,
+            'title': '{0}: {1}'.format(component, cb.Title),
+            'status': 'visible',
+            'klass': 'pp-row-default',
+            'panels': [
+                {
+                    'uuid': uid,
+                    'component': component,
+                    'grid-col': 12,
+                    'klass': 'panel-column panel-column-{0}'.format(component)
+                }
+            ]
+        }
+        return row
+
+    def _create_panel(self, component, key, value):
+        context = aq_inner(self.context)
+        token = django_random.get_random_string(length=24)
+        item = api.content.create(
+            type='ade25.panelpage.panel',
+            id=token,
+            title=token,
+            container=context,
+            safe_id=True
+        )
+        setattr(item, 'component', component)
+        if key == 'description':
+            setattr(item, 'textblock', value)
+        if key == 'headline':
+            setattr(item, 'textline', value)
+        else:
+            setattr(item, key, value)
+        modified(item)
+        item.reindexObject(idxs='modified')
+        uuid = api.content.get_uuid(obj=item)
+        return uuid
