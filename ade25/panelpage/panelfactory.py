9aae0e668289881505dd4ed960f5fa011664b9ff
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-05-15 01:00:16 +0200

    Experiment with nicer panel edit forms

--- ade25/panelpage/panelfactory.py
+++ ade25/panelpage/panelfactory.py
@@ -300,35 +300,39 @@ class PanelRichTextEditForm(form.SchemaEditForm):
         self.subpath.append(name)
         return self
 
+    def next_url(self):
+        context = aq_inner(self.context)
+        row = self.traverse_subpath[0]
+        url = '{0}/@@panelblock-editor/{1}'.format(
+            context.absolute_url(), row)
+        return url
+
+    def panel(self):
+        uid = self.traverse_subpath[2]
+        item = api.content.get(UID=uid)
+        return item
+
     @button.buttonAndHandler(_(u"Save"), name="save")
     def handleApply(self, action):
         data, errors = self.extractData()
         if errors:
             self.status = self.formErrorsMessage
             return
-        uid = self.traverse_subpath[2]
-        item = api.content.get(UID=uid)
+        item = self.panel()
         setattr(item, 'text', data['text'])
         modified(item)
         item.reindexObject(idxs='modified')
         IStatusMessage(self.request).addStatusMessage(
             _(u"The panel has successfully been updated"),
             type='info')
-        row = self.traverse_subpath[0]
-        context = aq_inner(self.context)
-        url = '{0}/@@panelblock-editor/{1}'.format(
-            context.absolute_url(), row)
-        return self.request.response.redirect(url)
+        return self.request.response.redirect(self.next_url())
 
     @button.buttonAndHandler(_(u"cancel"))
     def handleCancel(self, action):
-        context = aq_inner(self.context)
-        row = self.traverse_subpath[0]
-        url = '{0}/@@panelblock-editor/{1}'.format(context.absolute_url(), row)
         IStatusMessage(self.request).addStatusMessage(
             _(u"Content panel factory has been cancelled."),
             type='info')
-        return self.request.response.redirect(url)
+        return self.request.response.redirect(self.next_url())
 
     def getContent(self):
         uid = self.traverse_subpath[2]
