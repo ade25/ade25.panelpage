42f3c7a10e37eccacc029beed970993ca73dabf4
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-09-23 00:13:18 +0200

    Clean setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ setup(name='ade25.panelpage',
       version=version,
       description="Manage modular pages with blocks and panels",
       long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+          "Framework :: Plone",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
       keywords='',
       author='Ade25',
       author_email='cb@ade25.de',
@@ -40,10 +40,4 @@ setup(name='ade25.panelpage',
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      # The next two lines may be deleted after you no longer need
-      # addcontent support from paster and before you distribute
-      # your package.
-      setup_requires=["PasteScript"],
-      paster_plugins = ["ZopeSkel"],
-
       )
