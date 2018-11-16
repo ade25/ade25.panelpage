89d7a1b0d84a15008942647b1c31c622b4c85a0e
Christoph Böhner-Figas <boehner@team23.de>
2018-11-16 15:53:18 +0100

    Update storage behavior

---
+++ ade25/panelpage/behaviors/storage.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""Module providing behavior to store content panel data"""
+from plone import api
+from plone.autoform import directives as form_directives
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.dexterity.interfaces import IDexterityContent
+from plone.supermodel import directives as model_directives
+from plone.supermodel import model
+from plone.z3cform.textlines import TextLinesFieldWidget
+from zope import schema
+from zope.component import adapter
+from zope.interface import alsoProvides, implementer
+
+from ade25.panelpage import MessageFactory as _
+
+
+class IContentPanelStorage(model.Schema):
+    """Behavior providing a list of panel row strings parsable as JSON"""
+
+    model_directives.fieldset(
+        'panels',
+        label=u"Content Panels",
+        fields=['contentPanelsHeader',
+                'contentPanelsMain',
+                'contentPanelsFooter'
+                ]
+    )
+
+    if not api.env.debug_mode():
+        form_directives.omitted("contentPanelsFooter")
+
+    #form_directives.widget(contentPanelsHeader=TextLinesFieldWidget)
+    contentPanelsHeader = schema.List(
+        title=_("Content Panels Header"),
+        value_type=schema.TextLine(
+            title=_(u"Header Panel"),
+        ),
+        required=False,
+    )
+    #form_directives.widget(contentPanelsMain=TextLinesFieldWidget)
+    contentPanelsMain = schema.List(
+        title=_("Content Panels Main"),
+        value_type=schema.TextLine(
+            title=_(u"Main Panel"),
+        ),
+        required=False,
+    )
+    # form_directives.widget(contentPanelsFooter=TextLinesFieldWidget)
+    contentPanelsFooter = schema.List(
+        title=_("Content Panels Footer"),
+        value_type=schema.TextLine(
+            title=_(u"Footer Panel"),
+        ),
+        required=False,
+    )
+
+
+alsoProvides(IContentPanelStorage, IFormFieldProvider)
+
+
+@implementer(IContentPanelStorage)
+@adapter(IDexterityContent)
+class ContentPanelStorage(object):
+
+    def __init__(self, context):
+        self.context = context
--- ade25/panelpage/interfaces.py
+++ ade25/panelpage/interfaces.py
@@ -1,14 +1,6 @@
 # -*- coding: UTF-8 -*-
-from plone.app.textfield import RichText
-from plone.app.z3cform.widget import QueryStringFieldWidget
-from plone.autoform import directives, form
-from plone.namedfile.field import NamedBlobImage
-from plone.supermodel import model
-from zope import schema
 from zope.interface import Interface
 from plone.theme.interfaces import IDefaultPloneLayer
-
-from ade25.panelpage import MessageFactory as _
 
 
 class IAde25PanelPageLayer(IDefaultPloneLayer):
@@ -77,121 +69,6 @@ class IPanelTool(Interface):
         """
 
 
-class IPanelHeading(model.Schema):
-
-    textline = schema.TextLine(
-        title=_(u"Heading"),
-        required=False,
-    )
-
-
-class IPanelSubHeading(model.Schema):
-
-    textline = schema.TextLine(
-        title=_(u"Subheading"),
-        required=False,
-    )
-
-
-class IPanelAbstract(model.Schema):
-
-    textblock = schema.Text(
-        title=_(u"Abstract"),
-        required=False,
-    )
-
-
-class IPanelText(model.Schema):
-
-    textblock = schema.Text(
-        title=_(u"Plaintext"),
-        required=False,
-    )
-
-
-class IPanelRichText(model.Schema):
-
-    text = RichText(
-        title=_(u"Body"),
-        required=False,
-    )
-
-
-class IPanelImage(model.Schema):
-
-    image = NamedBlobImage(
-        title=_(u"Panel Image"),
-        description=_(u"Upload panel image suitable in size and "
-                      u"dimension for the usecase"),
-        required=False,
-    )
-
-
-class IPanelAlias(model.Schema):
-
-    alias = schema.TextLine(
-        title=_(u"Alias"),
-        description=_(u"Enter UID of content aliase that can be obtained by "
-                      u"appending @@UUID to a specific item URL"),
-        required=False,
-    )
-
-
-class IPanelListing(model.Schema):
-
-    contentlist = schema.Bool(
-        title=_(u"Show Content Listing"),
-        description=_(u"Enable to show a listing if this folder contents. All "
-                      u"query settings will be ignored if selected"),
-        required=False,
-    )
-    directives.widget('query', QueryStringFieldWidget)
-    query = schema.List(
-        title=_(u'Search terms'),
-        description=_(u"Define the search terms for the items you want "
-                      u"to list by choosing what to match on. "
-                      u"The list of results will be dynamically updated"),
-        value_type=schema.Dict(
-            value_type=schema.Field(),
-            key_type=schema.TextLine()),
-        required=False,
-        missing_value=''
-    )
-    sort_on = schema.TextLine(
-        title=_(u'label_sort_on', default=u'Sort on'),
-        description=_(u"Sort the collection on this index"),
-        required=False,
-    )
-
-    sort_reversed = schema.Bool(
-        title=_(u'label_sort_reversed', default=u'Reversed order'),
-        description=_(u'Sort the results in reversed order'),
-        required=False,
-    )
-
-    limit = schema.Int(
-        title=_(u'Limit'),
-        description=_(u'Limit Search Results'),
-        required=False,
-        default=1000,
-    )
-
-    item_count = schema.Int(
-        title=_(u'label_item_count', default=u'Item count'),
-        description=_(u'Number of items that will show up in one batch.'),
-        required=False,
-        default=30,
-    )
-
-    textline = schema.TextLine(
-        title=_(u"List Headline"),
-        description=_(u"Add optional list headline"),
-        required=False,
-    )
-
-    list_layout = schema.Choice(
-        title=_(u"List Layout"),
-        vocabulary=u"ade25.panelpage.AvailableLayouts",
-        required=False,
-        default=u'pp-list-base'
-    )
+class IContentPanelStorageSupport(Interface):
+    """ Marker for content panel storage support """
+    pass
